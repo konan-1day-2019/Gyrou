5b1621b15247cc06e9747e3042cc165a834f8836
Aki0326 <s1671108@s.konan-u.ac.jp>
2019-09-18 16:32:53 +0900

    Merge branch '0918fujii02' of https://github.com/konan-1day-2019/Gyrou into 0918fujii02
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/gyrou/IdentificationActivity.java
    #	app/src/main/java/com/example/gyrou/SelectTargetActivity.java
    #	app/src/main/java/com/example/gyrou/StatusPreviewActivity.java
    #	app/src/main/res/layout/activity_select_target.xml
    #	app/src/main/res/layout/activity_status_preview.xml

--- app/src/main/java/com/example/gyrou/SelectTargetActivity.java
+++ app/src/main/java/com/example/gyrou/SelectTargetActivity.java
@@ -6,13 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 
-public class SelectTargetActivity extends AppCompatActivity {
+public class SelectTargetActivity extends AppCompatActivity implements  SensorEventListener, Runnable
+{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_target);
+        SensorManager sensorManager;
+
+        float t = 0;
+
+        float sumZ = 0; // z軸の回転速度の合計値
+        float timeLimit = 60 * 5; // シェイクタイム
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_select_target);
+
+            sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
         // 本人確認から対象選択画面へ移行
         Button identificationButton = (Button) findViewById(R.id.identificationButton);
@@ -23,5 +37,57 @@ public class SelectTargetActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Sensor gyro = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        if (null != gyro)
+            sensorManager.registerListener(this, gyro, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event)
+    {
+
+        if(event.sensor.getType() == Sensor.TYPE_GYROSCOPE)
+           sumZ += event.values[1];
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy)
+    {
+
+    }
+
+    @Override
+    public  void run()
+    {
+        int period = 10;
+
+        while(!stopRun)
+        {
+            try
+            {
+                Thread.sleep(period);
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+                stopRun = true;
+            }
+
+            handler.post(new Runnable(){
+                @Override
+                public void run()
+                {
+                    long endTime = System.currentTimeMillis();
+
+                    long diffTime = (endTime - startTime);
+                }
+            })
+        }
     }
 }
