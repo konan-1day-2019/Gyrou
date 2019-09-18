fbcb2832e4e17c8d36a6f63f7e42ea8390d44cfa
Aki0326 <s1671108@s.konan-u.ac.jp>
2019-09-18 15:07:56 +0900

    本人確認～途中経過までの遷移

--- app/src/main/java/com/example/gyrou/SelectTargetActivity.java
+++ app/src/main/java/com/example/gyrou/SelectTargetActivity.java
@@ -2,7 +2,10 @@ package com.example.gyrou;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SelectTargetActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class SelectTargetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_target);
+
+        // 本人確認から対象選択画面へ移行
+        Button identificationButton = (Button) findViewById(R.id.identificationButton);
+
+        identificationButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(SelectTargetActivity.this, SelectTargetActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
